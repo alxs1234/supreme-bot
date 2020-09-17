8227c04631221d32b8e72f7e404e8924c0d53d12
alxs1234 <alx_sterling@yahoo.com>
2020-09-17 13:41:52 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+keys = {
+        "product_url": "",
+        "name": "",
+        "email": "",
+        "phone_number": "",
+        "street_address": "",
+        #"city": "", this is autofilled
+        "zip_code": "",
+        "card_cvv": "",
+        "card_number": ""
+}
